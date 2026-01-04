261f8b1b00172d5d7531ca1b09c06e57f449ae4e
gaimoy <106746080+Gaimoydev@users.noreply.github.com>
2026-01-04 12:56:58 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # ===== 源配置 =====
 # 全球
 URLS_GLOBAL=(
-  "https://proxyapi.sswc.cfd/api.php?key=0b2c885d4a23d97f"
+  "https://proxyapi.sswc.cfd/api.php?key=ay4t9b1w0s"
   "https://proxy.wuhen.shop/api/proxy?key=0b2c885d4a23d97f&limit=-1"
   "https://raw.githubusercontent.com/ClearProxy/checked-proxy-list/refs/heads/main/http/raw/all.txt"
 )
